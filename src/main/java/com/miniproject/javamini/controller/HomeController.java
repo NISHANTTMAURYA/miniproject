6f1fb15bb2c268e74df4f3305eb822e32b556667
NISHANTTMAURYA <mauryanishant2005@gmail.com>
2024-11-06 21:45:24 +0530

    hero page

--- src/main/java/com/miniproject/javamini/controller/HomeController.java
+++ src/main/java/com/miniproject/javamini/controller/HomeController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String redirectToroute() {
-
-        return "redirect:/create-route.html";
+    public String home() {
+        return "redirect:/index.html";
     }
 }
